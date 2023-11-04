ce38ba98c07352ffcaef4f4aafed984d2ac62e0f
Alexey Filippov <afilippov@acm.org>
2023-11-04 20:55:51 +0000

    Day 5 part 2

--- src/day5.rs
+++ src/day5.rs
@@ -1,10 +1,38 @@
+use std::collections::HashMap;
+
 fn nice_string(s: &str) -> bool {
     let bad_strings = ["ab", "cd", "pq", "xy"];
     let vowels = "aeiou";
     let nr_vowels = s.chars().filter(|&c| vowels.contains(c)).count();
-    let has_pairs = s.chars().collect::<Vec<char>>().windows(2).any(|pair| pair[0] == pair[1]);
+    let has_pairs = s
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(2)
+        .any(|pair| pair[0] == pair[1]);
     let bad = bad_strings.iter().any(|bad| s.contains(bad));
     nr_vowels >= 3 && has_pairs && !bad
+}
+
+fn nice_string_2(s: &str) -> bool {
+    let mut seen_pairs: HashMap<String, usize> = HashMap::new();
+    let mut good_pair = false;
+    let chars = &s.chars().collect::<Vec<char>>();
+    for i in 1..chars.len() {
+        let pair: String = chars[
+            i - 1..i + 1].iter().collect();
+        match seen_pairs.get(&pair) {
+            Some(&prev) if prev < i - 1 => {
+                good_pair = true;
+                break;
+            }
+            Some(_) => {}
+            None => {
+                seen_pairs.insert(pair, i);
+            }
+        }
+    }
+    let has_pairs = chars.windows(3).any(|pair| pair[0] == pair[2]);
+    good_pair && has_pairs
 }
 
 #[aoc_generator(day5)]
@@ -19,7 +47,7 @@ pub fn solve_part1(input: &Vec<String>) -> usize {
 
 #[aoc(day5, part2)]
 pub fn solve_part2(input: &Vec<String>) -> usize {
-    2
+    input.iter().filter(|s| nice_string_2(s)).count()
 }
 
 #[cfg(test)]
@@ -27,11 +55,19 @@ mod tests {
     use super::*; // Bring the outer module's functions into the scope of the tests module
 
     #[test]
-    fn from_task_description() {
+    fn part_1_description() {
         assert_eq!(true, nice_string("ugknbfddgicrmopn"));
         assert_eq!(true, nice_string("aaa"));
         assert_eq!(false, nice_string("jchzalrnumimnmhp"));
         assert_eq!(false, nice_string("haegwjzuvuyypxyu"));
         assert_eq!(false, nice_string("dvszwmarrgswjxmb"));
+    }
+
+    #[test]
+    fn part_2_description() {
+        assert_eq!(true, nice_string_2("qjhvhtzxzqqjkmpb"));
+        assert_eq!(true, nice_string_2("xxyxx"));
+        assert_eq!(false, nice_string_2("uurcxstgmygtbstg"));
+        assert_eq!(false, nice_string_2("ieodomkazucvgmuy"));
     }
 }
