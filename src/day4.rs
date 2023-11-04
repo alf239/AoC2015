4e1ccc293ad74bae6d65fce587150b39ee813876
Alexey Filippov <afilippov@acm.org>
2023-11-04 19:44:55 +0000

    Day 4 Part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,19 +1,23 @@
 use md5;
 
-#[aoc(day4, part1)]
-pub fn solve_part1(input: &str) -> Option<u32> {
+fn just_solve_it(secret_key: &str, fingerprint: &str) -> Option<u32> {
     for i in 0.. {
-        let formatted = format!("{}{}", input, i);
+        let formatted = format!("{}{}", secret_key, i);
         let hash = md5::compute(formatted);
         let str_hash = format!("{:x}", hash);
-        if str_hash.starts_with("00000") {
+        if str_hash.starts_with(fingerprint) {
             return Some(i);
         }
     }
     None
 }
 
+#[aoc(day4, part1)]
+pub fn solve_part1(input: &str) -> Option<u32> {
+    just_solve_it(input, "00000")
+}
+
 #[aoc(day4, part2)]
-pub fn solve_part2(input: &str) -> usize {
-    2
+pub fn solve_part2(input: &str) -> Option<u32> {
+    just_solve_it(input, "000000")
 }
