ff66ac9de05dd9d946bf74a60b8119bed3d63bc7
Alexey Filippov <afilippov@acm.org>
2023-11-11 15:38:51 +0000

    Day 12 part 2

--- src/day12.rs
+++ src/day12.rs
@@ -1,27 +1,36 @@
-use serde_json::Value;
+use serde_json::{Map, Value};
 
 #[aoc_generator(day12)]
 pub fn input_generator(input: &str) -> Value {
     serde_json::from_str(input).unwrap()
 }
 
-fn sum_of_ints(input: &Value) -> i64 {
+fn sum_of_ints<F>(input: &Value, p: &F) -> i64
+where
+    F: Fn(&Map<String, Value>) -> bool,
+{
     match input {
         Value::Null => 0,
         Value::Bool(_) => 0,
         Value::Number(n) => n.as_i64().unwrap(),
-        Value::Object(vals) => vals.values().map(|v| sum_of_ints(v)).sum(),
-        Value::Array(vals) => vals.into_iter().map(|v| sum_of_ints(v)).sum(),
-        Value::String(_) => 0
+        Value::Object(vals) if p(vals) => vals.values().map(|v| sum_of_ints(v, p)).sum(),
+        Value::Object(_) => 0,
+        Value::Array(vals) => vals.into_iter().map(|v| sum_of_ints(v, p)).sum(),
+        Value::String(_) => 0,
     }
 }
 
 #[aoc(day12, part1)]
 pub fn solve_part1(input: &Value) -> i64 {
-    sum_of_ints(input)
+    sum_of_ints(input, &|_| true)
 }
 
 #[aoc(day12, part2)]
 pub fn solve_part2(input: &Value) -> i64 {
-    2
+    sum_of_ints(input, &|m| {
+        !m.values().any(|v| match v {
+            Value::String(s) => s == "red",
+            _ => false,
+        })
+    })
 }
