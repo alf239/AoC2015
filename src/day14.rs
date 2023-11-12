ac3a1d1f12b19f69f6e11a03610ff5a5a6cc5b05
Alexey Filippov <afilippov@acm.org>
2023-11-12 13:59:41 +0000

    Dear deer

--- src/day14.rs
+++ src/day14.rs
@@ -2,14 +2,20 @@ use std::cmp::min;
 
 use itertools::Itertools;
 
+pub struct Deer {
+    speed: i64,
+    streak: i64,
+    cooldown: i64,
+}
+
 #[aoc_generator(day14)]
-pub fn input_generator(input: &str) -> Vec<(i64, i64, i64)> {
+pub fn input_generator(input: &str) -> Vec<Deer> {
     input
         .lines()
         .map(|l| {
             let mut attitude = l.trim().split(' ');
             // Rudolph can fly 22 km/s for 8 seconds, but then must rest for 165 seconds.
-            let name = attitude.next().unwrap(); // Rudolph
+            attitude.next(); // Rudolph
             attitude.next(); // can
             attitude.next(); // fly
             let speed: i64 = attitude.next().unwrap().parse().unwrap(); // 22
@@ -24,25 +30,29 @@ pub fn input_generator(input: &str) -> Vec<(i64, i64, i64)> {
             attitude.next(); // for
             let cooldown: i64 = attitude.next().unwrap().parse().unwrap(); // 165
             attitude.next(); // seconds
-            (speed, streak, cooldown)
+            Deer {
+                speed,
+                streak,
+                cooldown,
+            }
         })
         .collect_vec()
 }
 
-fn distance(speed: i64, streak: i64, cooldown: i64, t: i64) -> i64 {
-    let cycle = streak + cooldown;
+fn distance(d: &Deer, t: i64) -> i64 {
+    let cycle = d.streak + d.cooldown;
     let full_cycles = t / cycle;
     let remainder = t % cycle;
-    let flight = full_cycles * streak + min(streak, remainder);
-    flight * speed
+    let flight = full_cycles * d.streak + min(d.streak, remainder);
+    flight * d.speed
 }
 
 #[aoc(day14, part1)]
-pub fn solve_part1(input: &Vec<(i64, i64, i64)>) -> i64 {
-    input.iter().map(|(sp, st, cd)| distance(*sp, *st, *cd, 2503)).max().unwrap()
+pub fn solve_part1(input: &Vec<Deer>) -> i64 {
+    input.iter().map(|d| distance(d, 2503)).max().unwrap()
 }
 
 #[aoc(day14, part2)]
-pub fn solve_part2(input: &Vec<(i64, i64, i64)>) -> i64 {
+pub fn solve_part2(input: &Vec<Deer>) -> i64 {
     2
 }
